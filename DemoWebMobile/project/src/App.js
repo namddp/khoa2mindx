9acea185287fba9b0884e6d07f2368bb6fad6a3b
Đức Huy <briannguyen.contact@gmail.com>
2023-02-09 19:24:53 +0700

    update

--- DemoWebMobile/project/src/App.js
+++ DemoWebMobile/project/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from "react-router-dom";
 import Header from "./header/Header"
 import RunningText from "./header/RunningText"
--- DemoWebMobile/project/src/policy/Policy.js
+++ DemoWebMobile/project/src/policy/Policy.js
@@ -3,40 +3,41 @@ import "./Policy.css";
 
 function Policy() {
   return (
-    <ul className="list">
+    <ul className="list " >
       <li className="list-policy">
         <i className="fa-solid fa-truck-fast fa-icon"></i>
         <div className="ship-info">
           SHIP SIÊU TỐC - 2H <br />
-          Miễn phí - An Toàn
+          <p className="text-sm">Miễn phí - An Toàn</p>
         </div>
       </li>
       <li className="list-policy">
-      <i className="fa-solid fa-phone fa-icon "></i>
+        <i className="fa-solid fa-phone fa-icon "></i>
         <div className="ship-info">
-         TƯ VẤN MIỄN PHÍ <br />
-         Từ 7h-24h: 1900.63.60.99
+          TƯ VẤN MIỄN PHÍ <br />
+          <p className="text-sm"> Từ 7h-24h: 1900.63.60.99</p>
         </div>
       </li>{" "}
       <li className="list-policy">
         <i className="  fa-solid fa-rotate-left fa-icon"></i>
         <div className="ship-info">
-         HOÀN TRẢ MIỄN PHÍ <br />
-         Lên đến 45 ngày
+          HOÀN TRẢ MIỄN PHÍ <br />
+          <p className="text-sm"> Lên đến 45 ngày</p>
         </div>
       </li>{" "}
       <li className="list-policy">
-        <i className="fa-solid fa-truck-fast fa-icon"></i>
+        <i class="fa-solid fa-credit-card fa-icon"></i>
         <div className="ship-info">
-         MIỄN PHÍ CÀ THẺ  <br />
-         Visa Master ATM
+          MIỄN PHÍ CÀ THẺ <br />
+          <p className="text-sm"> Visa Master ATM</p>
         </div>
       </li>{" "}
-      <li className="list-policy">
-        <i className="fa-solid fa-truck-fast fa-icon"></i>
+      <li className="list-policy ">
+        <i class="fa-solid fa-percent fa-icon "></i>
         <div className="ship-info">
-          TRẢ GÓP 0% LÃI SUẤT<br />
-          Online - Tận nhà
+          TRẢ GÓP 0% LÃI SUẤT
+          <br />
+          <p className="text-sm"> Online - Tận nhà</p>
         </div>
       </li>
     </ul>
