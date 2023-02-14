1da4c2b51c3ec8828dce4710f2ac41c0d708da13
Đức Huy <briannguyen.contact@gmail.com>
2023-02-14 16:33:41 +0700

    update

--- DemoWebMobile/project/src/components/header/SearchBar.js
+++ DemoWebMobile/project/src/components/header/SearchBar.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-import { ProductsData } from "../../data/ProductsData";
-=======
 import React, { useState ,useEffect} from "react";
 import {ProductsData} from "../../data/ProductsData";
->>>>>>> 80e90eabd4688c3447f47901e0739f24cbf05b58
 import "./SearchBar.css";
 
 function SearchBar() {
@@ -20,23 +15,11 @@ function SearchBar() {
     console.log(filteredData)
     setFilteredData(
       ProductsData.filter((product) =>
-<<<<<<< HEAD
         new RegExp(searchTerm, "i").test(product.name)
       )
     );
-  };
+  });
 
-=======
-      {
-        if (searchTerm === '' || searchTerm === null) {
-          return [];
-      }
-      else {
-          return product.name.toLowerCase().includes(searchTerm)
-      }}));
-      console.log(filteredData)
-  }, [searchTerm]);
->>>>>>> 80e90eabd4688c3447f47901e0739f24cbf05b58
   return (
     <div>
       <li className="header-search">
@@ -53,7 +36,6 @@ function SearchBar() {
         </form>
       </li>
       <ul className="product-list">
-<<<<<<< HEAD
         {searchTerm ? (
           filteredData.length > 0 ? (
             filteredData.map((product) => (
@@ -68,20 +50,6 @@ function SearchBar() {
           ) : (
             <p className="not-found">Không Tìm Thấy Sản Phẩm Liên Quan</p>
           )
-=======
-        {searchTerm.length > 0 ? (
-          filteredData.map((product) => (
-            <li className="product-frame" key={product.id}>
-              <img src={product.image[0]} alt={product.name} />
-              <div>
-              <h5>{product.name}</h5>
-              <p>{product.ram_options[0].info[0].price}</p>
-              </div>
-            </li>
-          ))
-        ) : searchTerm ? (
-          <p className="not-found">This item not found</p>
->>>>>>> 80e90eabd4688c3447f47901e0739f24cbf05b58
         ) : null}
         
       </ul>
