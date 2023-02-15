83ff6cc8a655cc9f1c94ae617945afd3a09c0c1e
TMHieu <hieu.truongminh.hcm@gmail.com>
2023-02-15 14:14:11 +0700

    data phụ kiện

--- DemoWebMobile/project/src/data/ProductsData.jsx
+++ DemoWebMobile/project/src/data/ProductsData.jsx
@@ -303,4 +303,235 @@ export const ProductsData = [
       "https://product.hstatic.net/200000544803/product/5_f319c002070540ad93c60b241fd951d4_master.png",
     ],
   },
+  {
+    id: 9,
+    type: "Ipad",
+    name: "iPad mini 6 2021",
+    serial: "AMOT00145-72",
+    brand: "Apple",
+    color: ["gray", "white", "pink", "violet"],
+    ram_options: [
+      {
+        ram: "8GB",
+        info: [
+          {
+            rom: "64GB",
+            price: 14900000,
+          },
+          {
+            rom: "256GB",
+            price: 19920000,
+          },
+        ],
+      },
+    ],
+    image: [
+      "https://product.hstatic.net/200000544803/product/11_124d5877446f40328f1741ae4d306f4e_master.png",
+      "https://product.hstatic.net/200000544803/product/13_27c398aae9b44eac8b384b0a34f5f8ac_master.png",
+      "https://product.hstatic.net/200000544803/product/14_c6226676413643818efdbd8e89692500_master.jpg",
+      "https://product.hstatic.net/200000544803/product/15_bbbcb55b7c1e4c6e953e1d08683a9817_master.png",
+    ],
+  },
+  {
+    id: 10,
+    type: "Ipad",
+    name: "iPad Pro 12.9 inch (M1, 2021)",
+    serial: "AMOT00145-72",
+    brand: "Apple",
+    color: ["gray", "white"],
+    ram_options: [
+      {
+        ram: "8GB",
+        info: [
+          {
+            rom: "128",
+            price: 33900000,
+          },
+          {
+            rom: "256GB",
+            price: 37900000,
+          },
+        ],
+      },
+    ],
+    image: [
+      "https://product.hstatic.net/200000544803/product/2_279595eebf0c4fe4a18de944e23f56e2_master.png",
+      "https://product.hstatic.net/200000544803/product/3_7e6204a551f04c359c807e74f001d9d7_master.png",
+      "https://product.hstatic.net/200000544803/product/1_080c613d0b0c48638a1995668549866c_master.png",
+      "https://product.hstatic.net/200000544803/product/6_d73aa2bd5d4c4118a2ec97adbb54a837_master.png",
+    ],
+  },
+  {
+    id: 11,
+    type: "Macbook",
+    name: "MacBook Air M2 2022",
+    serial: "AMOT00145-72",
+    brand: "Apple",
+    color: ["gray", "black", "white", "sliver"],
+    ram_options: [
+      {
+        ram: "8GB",
+        info: [
+          {
+            rom: "256GB",
+            price: 32900000,
+          },
+          {
+            rom: "512GB",
+            price: 38490000,
+          },
+        ],
+      },
+      {
+        ram: "12GB",
+        info: [
+          {
+            rom: "256GB",
+            price: 39490000,
+          },
+          {
+            rom: "512GB",
+            price: 49490000,
+          },
+        ],
+      },
+    ],
+    image: [
+      "https://product.hstatic.net/200000544803/product/1_885ed9f043f04a3ba9ba7c6fe0a0fcb7_master.png",
+      "https://product.hstatic.net/200000544803/product/2_3df7871fb13c49f7881773aec09f323e_master.png",
+      "https://product.hstatic.net/200000544803/product/3_125cd3306003464fb3fa74181547cbce_master.png",
+      "https://product.hstatic.net/200000544803/product/5_2b9d66e507504bdb8711267e129a2161_master.png",
+    ],
+  },
+  {
+    id: 12,
+    type: "Macbook",
+    name: "iMac M1 2021 24 inch",
+    serial: "AMOT00145-72",
+    brand: "Apple",
+    color: ["pink", "green", "blue", "sliver"],
+    ram_options: [
+      {
+        ram: "12GB",
+        info: [
+          {
+            rom: "512GB",
+            price: 31290000,
+          },
+          {
+            rom: "1T",
+            price: 34290000,
+          },
+        ],
+      },
+    ],
+    image: [
+      "https://product.hstatic.net/200000544803/product/8_436f08de65054356a01397b5ff7e07d4_master.png",
+      "https://product.hstatic.net/200000544803/product/9_e3eb0f218b1d47369fbcfd97a4c995e8_master.png",
+      "https://product.hstatic.net/200000544803/product/11_e3e72517e546480cb768d696975f8461_master.jpg",
+      "https://product.hstatic.net/200000544803/product/12_8c376a439c6643279790d3f1f6ada865_master.png",
+    ],
+  },
+  {
+    id: 13,
+    type: "Apple Watch",
+    name: "Apple Watch Series 3",
+    serial: "AMOT00145-72",
+    brand: "Apple",
+    color: ["gray", "sliver"],
+    size: ["42mm"],
+    price: 6990000,
+    image: [
+      "https://product.hstatic.net/200000544803/product/2_2ce338f3ae6c4fc1a21733773d52324d_master.png",
+      "https://product.hstatic.net/200000544803/product/1_c8513dfd8368450c911385dfd81144a6_master.png",
+      "https://product.hstatic.net/200000544803/product/3_4b0e0bb074c44db79d98e75feedb3383_master.png",
+      "https://product.hstatic.net/200000544803/product/4_dd6aba12feed43e3a5cff86df9a315e5_master.png",
+    ],
+  },
+  {
+    id: 14,
+    type: "Apple Watch",
+    name: "Apple Watch SE",
+    serial: "AMOT00145-72",
+    brand: "Apple",
+    color: ["gray", "sliver"],
+    size: ["40mm"],
+    price: 8990000,
+    image: [
+      "https://product.hstatic.net/200000544803/product/7_a79d10a5d24040419cc61dd339f5571a_master.png",
+      "https://product.hstatic.net/200000544803/product/1_d76aa7adb12644e989ba9cc21a509984_master.png",
+      "https://product.hstatic.net/200000544803/product/2_d49dd3502a5d401ba86325bc0af166ed_master.png",
+      "https://product.hstatic.net/200000544803/product/3_6c9ac51f3ddb4b92a7c0c529b63ca0c8_master.png",
+    ],
+  },
+  {
+    id: 15,
+    type: "Airpod",
+    name: "AirPods (gen 3)",
+    serial: "AMOT00145-72",
+    brand: "Apple",
+    price: 5490000,
+    image: [
+      "https://product.hstatic.net/200000544803/product/1_e9d2c821129845038664bfe32d6f26f4_master.png",
+      "https://product.hstatic.net/200000544803/product/2_ffc814354fe04175b11297afffbe4cca_master.png",
+      "https://product.hstatic.net/200000544803/product/3_03f28fb7211941dbb4eeedb430e42c81_master.png",
+      "https://product.hstatic.net/200000544803/product/4_b64a1a33f8184848ba5fdf172bb3be95_master.png",
+    ],
+  },
+  {
+    id: 16,
+    type: "Airpod",
+    name: "AirPods 2",
+    serial: "AMOT00145-72",
+    brand: "Apple",
+    price: 4390000,
+    image: [
+      "https://product.hstatic.net/200000544803/product/1_e85e13e8773a456092fac446c069eb31_master.png",
+      "https://product.hstatic.net/200000544803/product/2_8c4468502f824da8aa9d5b7db50a03f1_master.png",
+      "https://product.hstatic.net/200000544803/product/3_bf697910e9364181978a51140ca67230_master.png",
+      "https://product.hstatic.net/200000544803/product/4_a51da66e3aa94a5da536671b483c403d_master.png",
+    ],
+  },
+  {
+    id: 17,
+    type: "AirTag",
+    name: "Apple AirTag (1 Pack)",
+    serial: "AMOT00145-72",
+    brand: "Apple",
+    price: 2990000,
+    image: [
+      "https://product.hstatic.net/200000544803/product/1_17f5a705b63e465290ea81c157b7b45c_master.png",
+      "https://product.hstatic.net/200000544803/product/2_f2f3d2923d99475e9a451cb4483257f9_master.png",
+      "https://product.hstatic.net/200000544803/product/3_e997f93b7f284619adfa4cc5b57198b7_master.png",
+      "https://product.hstatic.net/200000544803/product/4_05d390d92194486aa1d7a25426f9149e_master.png",
+    ],
+  },
+  {
+    id: 18,
+    type: "AirTag",
+    name: "Apple AirTag (1 Pack)",
+    serial: "AMOT00145-72",
+    brand: "Apple",
+    price: 2990000,
+    image: [
+      "https://product.hstatic.net/200000544803/product/1_17f5a705b63e465290ea81c157b7b45c_master.png",
+      "https://product.hstatic.net/200000544803/product/2_f2f3d2923d99475e9a451cb4483257f9_master.png",
+      "https://product.hstatic.net/200000544803/product/3_e997f93b7f284619adfa4cc5b57198b7_master.png",
+      "https://product.hstatic.net/200000544803/product/4_05d390d92194486aa1d7a25426f9149e_master.png",
+    ],
+  },
+  {
+    id: 19,
+    type: "SDP",
+    name: "Sạc dự phòng không dây",
+    serial: "AMOT00145-72",
+    brand: "Apple",
+    price: 2990000,
+    image: [
+      "https://product.hstatic.net/200000544803/product/mjwy3_35bd8123b09e471595b02553a7683a24_master.jpg",
+      "https://product.hstatic.net/200000544803/product/mjwy3_av1_2941563c86ef4abf9745642cce054c56_master.jpg",
+      "https://product.hstatic.net/200000544803/product/mjwy3_av2_753c4328f0554eb8baf44327d7c92dac_master.jpg",
+      "https://product.hstatic.net/200000544803/product/mjwy3_av3_66dc2d92f5734b6e997dadf2111ccc85_master.jpg",
+    ],
+  },
 ];
