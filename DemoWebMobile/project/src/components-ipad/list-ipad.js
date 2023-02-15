7b114cdd8481801b75867f23f914517e71b15fee
TMHieu <hieu.truongminh.hcm@gmail.com>
2023-02-15 12:39:29 +0700

    dataipad

--- DemoWebMobile/project/src/components-ipad/list-ipad.js
+++ DemoWebMobile/project/src/components-ipad/list-ipad.js
@@ -4,39 +4,41 @@ import "./list-ipad.css";
 
 function ListIpad(props) {
   return (
-    <div className="listIpad-container w-[300px] h-[500px] border-solid border-[1px] rounded-[20px] text-center ml-[10px] p-[5px] shadow-xl hover:shadow-gray-500">
-      <div className="text-center h-[300px] w-[300px]">
+    <div className="listIpad-container w-[265px] h-[450px] border-solid border-[1px] rounded-[20px] text-center ml-[10px] p-[5px] shadow-xl hover:shadow-gray-500">
+      <div className="text-center h-[265px] w-[265px]">
         <img
-          className=" img1 w-auto h-[250px] m-auto "
+          className="img1 w-auto h-[210px] m-auto "
           src={props.image[0]}
           alt=""
         />
         <img
-          className="img2 w-auto h-[250px] m-auto hidden"
+          className="img2 w-auto h-[210px] m-auto hidden"
           src={props.image[1]}
           alt=""
         />
       </div>
       <div className="flex justify-center ">
         {props.image.map((img, index) => {
-          return <img className="h-[70px] w-[70px]" src={img} alt="" />;
+          return <img className="h-[60px] w-[60px]" src={img} alt="" />;
         })}
       </div>
-      <div className="text-left">
-        <h1>{props.name}</h1>
+      <div className="text-left ">
+        <h5 class="text-lg">{props.name}</h5>
       </div>
-      <div className="flex">
-        <h5>13,590,000₫</h5>
-        <h5 className="ml-[20%] line-through">16,990,000₫</h5>
+      <div className="flex mt-[30px]">
+        <p>13,590,000₫</p>
+        <p className="ml-[15px] line-through">16,990,000₫</p>
       </div>
     </div>
   );
 }
+
 const dataIpad = ProductsData.filter((a) => a.type === "Ipad");
 console.log(dataIpad[0].ram_options[0]);
+
 function ListIpads() {
   return (
-    <div className="grid grid-cols-5 gap-[30%]">
+    <div className="flex justify-center gap-[1px]">
       {dataIpad.map((item, index) => {
         return (
           <ListIpad
--- DemoWebMobile/project/src/data/ProductsData.jsx
+++ DemoWebMobile/project/src/data/ProductsData.jsx
@@ -1,231 +1,364 @@
 export const ProductsData = [
-    {
-        id : 0,
-        type: 'Macbook',
-        name : 'MacBook Air M1 2020',
-        serial : 'AMOT001-7',
-        brand : 'Apple',
-        color : ['gray','white','golden'],
-        ram_options : [{
-            ram : '8GB',
-            info: [{
-                rom: "256GB",
-                price: 28990000,
-            },{
-                rom: '512GB',
-                price: 34990000,
-            }]
-        },{
-            ram : '16GB',
-            info: [{
-                rom: '256GB',
-                price: 33999000,
-            },{
-                rom: '512GB',
-                price: 39999000,
-            },{
-                rom: '1TB',
-                price: 59999000,
-            }]
-        }],
-        image: ['https://product.hstatic.net/200000544803/product/10_e4b6175708414bdc8f4c1d152795f908_master.png',
-        'https://product.hstatic.net/200000544803/product/11_46baafebee87496296f94931d01f7d70_master.png'
-        ,'https://product.hstatic.net/200000544803/product/12_c146dde7313f44309dbc3aeadbdfdcd1_master.png'
-        ,'https://product.hstatic.net/200000544803/product/13_8492d6b1324e46fcaf1820a1f75a839f_master.png']
-    },
-    {
-        id : 1,
-        type: 'Macbook',
-        name : 'MacBook Air M2 2022',
-        serial : 'AMOT001-8',
-        brand : 'Apple',
-        color : ['gray','white','golden'],
-        ram_options : [{
-            ram : '8GB',
-            info: [{
-                rom: "256GB",
-                price: 32990000,
-            },{
-                rom: '512GB',
-                price: 39990000,
-            }]
-        },{
-            ram : '16GB',
-            info: [{
-                rom: '256GB',
-                price: 39490000,
-            },{
-                rom: '512GB',
-                price: 49490000,
-            }]
-        }],
-        image: ['https://product.hstatic.net/200000544803/product/6_1b78619eb90f4f96a1113bf7c9805638_master.png',
-        'https://product.hstatic.net/200000544803/product/7_8bc779722cbc42d5b03f56990f6e3218_master.jpg'
-        ,'https://product.hstatic.net/200000544803/product/10_be7dc54cb7f24c2c8d3028472c85043c_master.jpg'
-        ,'https://product.hstatic.net/200000544803/product/14_a15f6a7a4a6347458d6d5ce9d42102c1_master.jpg']
-    },
-    {
-        id : 2,
-        type: 'Macbook',
-        name : 'MacBook Pro M1 2020',
-        serial : 'AMOT001-9',
-        brand : 'Apple',
-        color : ['gray','white'],
-        ram_options : [{
-            ram : '8GB',
-            info: [{
-                rom: "256GB",
-                price: 32000000,
-            },{
-                rom: '512GB',
-                price: 33990000,
-            }]
-        },{
-            ram : '16GB',
-            info: [{
-                rom: '256GB',
-                price: 35999000,
-            },{
-                rom: '512GB',
-                price: 42999000,
-            }]
-        }],
-        image: ['https://product.hstatic.net/200000544803/product/6_ae4de0c675944598ae6553b33932574c_master.png',
-        'https://product.hstatic.net/200000544803/product/7_68ae9af150e343298d0c94bc26430398_master.png'
-        ,'https://product.hstatic.net/200000544803/product/8_34a103c096cb47c48f1e7ffb1eed1834_master.png'
-        ,'https://product.hstatic.net/200000544803/product/11_f8527e5811bf4f489cd13f594bd0154e_master.png']
-    },
-    {
-        id : 3,
-        type: 'Iphone',
-        name : 'iPhone 11',
-        serial : 'AMOT002-1',
-        brand : 'Apple',
-        color : ['gray','white','golden','black','magenta','purple'],
-        ram_options : [{
-            ram : '4GB',
-            info: [{
-                rom: '64GB',
-                price: 18990000,
-            },{
-                rom: '128GB',
-                price: 19990000,
-            }]
-        }],
-        image: ['https://product.hstatic.net/200000544803/product/5_0cb452551d0743a6a430cc753c4b97e1_master.png',
-        'https://product.hstatic.net/200000544803/product/6_1eb57a070fec4692b53137cfa533f804_master.png'
-        ,'https://product.hstatic.net/200000544803/product/8_cfedc371d4df49a09a86c6d9d2d006f8_master.png'
-        ,'https://product.hstatic.net/200000544803/product/14_f1a49f44d2a04d6e8bd6f8d602fb960f_master.png']
-    },
-    {
-        id : 4,
-        type: 'Iphone',
-        name : 'iPhone 12',
-        serial : 'AMOT002-74',
-        brand : 'Apple',
-        color : ['gray','white','golden','black','magenta','purple'],
-        ram_options : [{
-            ram : '4GB',
-            info: [{
-                rom: '64GB',
-                price: 22999000,
-            },{
-                rom: '128GB',
-                price: 24999000,
-            },{
-                rom: '256GB',
-                price: 28999000,
-            }]
-        }],
-        image: ['https://product.hstatic.net/200000544803/product/6_74c8a72a363d459fbd7e54bd6731639b_master.png',
-        'https://product.hstatic.net/200000544803/product/7_871a5d63d5a64b18a138179fa49b1b5d_master.png'
-        ,'https://product.hstatic.net/200000544803/product/8_093c6b198b274d72a620a153c5587160_master.png'
-        ,'https://product.hstatic.net/200000544803/product/9_244f9920f3654af1abc89fe3ea471193_master.png']
-    },
-    {
-        id : 5,
-        type: 'Iphone',
-        name : 'iPhone 13',
-        serial : 'AMOT0041-72',
-        brand : 'Apple',
-        color : ['gray','white','golden'],
-        ram_options : [{
-            ram : '8GB',
-            info: [{
-                rom: "256GB",
-                price: 24990000,
-            },{
-                rom: '512GB',
-                price: 27990000,
-            }]
-        }]
-        ,
-        image: ['https://product.hstatic.net/200000544803/product/iphone_13_green_pdp_position-1b_a7d90e812d004d17812976b95c859ddf_master.png',
-        'https://product.hstatic.net/200000544803/product/iphone_13_green_pdp_position-2_s_a04661f3fc244dfda82a9c2f22701948_master.png'
-        ,'https://product.hstatic.net/200000544803/product/iphone_13_green_pdp_position-4_d_5c914bd089dd4c8c842e74a3ea7641a5_master.png'
-        ,'https://product.hstatic.net/200000544803/product/iphone_13_green_pdp_position-9_t_b450ce81f8d24bca89d4019cc88d9096_master.png']
-    },
-    {
-        id : 6,
-        type: 'Ipad',
-        name : 'iPad Air 4',
-        serial : 'AMOT041-412',
-        brand : 'Apple',
-        color : ['gray','white','golden','magenta','black','green'],
-        ram_options :[{
-            ram : '8GB',
-            info: [{
-                rom: "256GB",
-                price: 16990000,
-            },{
-                rom: '512GB',
-                price: 20990000,
-            }]
-        }],
-        image: ['https://product.hstatic.net/200000544803/product/2_a884f7f7c3464c1585ea11a83cb425a4_master.png',
-        'https://product.hstatic.net/200000544803/product/3_ab6a029bbe9f4ebf8c8c0391e7a2e1e6_master.png'
-        ,'https://product.hstatic.net/200000544803/product/14_8e186b1ad8454684acb431cb4bb74d8c_master.png'
-        ,'https://product.hstatic.net/200000544803/product/111_41d2a9a3fd234490af4c7cf435d1611c_master.png']
-    },
-    {
-        id : 7,
-        type: 'Ipad',
-        name : 'iPad Air 5',
-        serial : 'AMOT0021-74',
-        brand : 'Apple',
-        color : ['gray','white','golden'],
-        ram_options : [{
-            ram : '8GB',
-            info: [{
-                rom: "64GB",
-                price: 17500000,
-            }]
-    }],
-        image: ['https://product.hstatic.net/200000544803/product/d_c9a314402eb445ed91f296044aa8b90c_master.png',
-        'https://product.hstatic.net/200000544803/product/ipad_air_cellular_space_gray_pdp_6af6332fe55f46ef82ea8a9de3d95ddc_master.png'
-        ,'https://product.hstatic.net/200000544803/product/x_42ffebff4e594598b52f213c5e40b688_master.png'
-        ,'https://product.hstatic.net/200000544803/product/sadasd_fb511cb4ea224a0da020ac7ffd02fe7d_master.png']
-    },
-    {
-        id : 8,
-        type: 'Ipad',
-        name : 'iPad gen 9',
-        serial : 'AMOT00145-72',
-        brand : 'Apple',
-        color : ['gray','white'],
-        ram_options : [{
-            ram : '8GB',
-            info: [{
-                rom: "64GB",
-                price: 10990000,
-            },{
-                rom: '256GB',
-                price: 13990000,
-            }]
-        }],
-        image: ['https://product.hstatic.net/200000544803/product/2_f5e2c59e1b9e453aa078c1c640fe016f_master.png',
-        'https://product.hstatic.net/200000544803/product/3_bb5c55f65660435fa82a780db46e4bfd_master.png'
-        ,'https://product.hstatic.net/200000544803/product/4_6e2f37547ced4abd861a26492d201539_master.png'
-        ,'https://product.hstatic.net/200000544803/product/5_f319c002070540ad93c60b241fd951d4_master.png']
-    }
-]
+  {
+    id: 0,
+    type: "Macbook",
+    name: "MacBook Air M1 2020",
+    serial: "AMOT001-7",
+    brand: "Apple",
+    color: ["gray", "white", "golden"],
+    ram_options: [
+      {
+        ram: "8GB",
+        info: [
+          {
+            rom: "256GB",
+            price: 28990000,
+          },
+          {
+            rom: "512GB",
+            price: 34990000,
+          },
+        ],
+      },
+      {
+        ram: "16GB",
+        info: [
+          {
+            rom: "256GB",
+            price: 33999000,
+          },
+          {
+            rom: "512GB",
+            price: 39999000,
+          },
+          {
+            rom: "1TB",
+            price: 59999000,
+          },
+        ],
+      },
+    ],
+    image: [
+      "https://product.hstatic.net/200000544803/product/10_e4b6175708414bdc8f4c1d152795f908_master.png",
+      "https://product.hstatic.net/200000544803/product/11_46baafebee87496296f94931d01f7d70_master.png",
+      "https://product.hstatic.net/200000544803/product/12_c146dde7313f44309dbc3aeadbdfdcd1_master.png",
+      "https://product.hstatic.net/200000544803/product/13_8492d6b1324e46fcaf1820a1f75a839f_master.png",
+    ],
+  },
+  {
+    id: 1,
+    type: "Macbook",
+    name: "MacBook Air M2 2022",
+    serial: "AMOT001-8",
+    brand: "Apple",
+    color: ["gray", "white", "golden"],
+    ram_options: [
+      {
+        ram: "8GB",
+        info: [
+          {
+            rom: "256GB",
+            price: 32990000,
+          },
+          {
+            rom: "512GB",
+            price: 39990000,
+          },
+        ],
+      },
+      {
+        ram: "16GB",
+        info: [
+          {
+            rom: "256GB",
+            price: 39490000,
+          },
+          {
+            rom: "512GB",
+            price: 49490000,
+          },
+        ],
+      },
+    ],
+    image: [
+      "https://product.hstatic.net/200000544803/product/6_1b78619eb90f4f96a1113bf7c9805638_master.png",
+      "https://product.hstatic.net/200000544803/product/7_8bc779722cbc42d5b03f56990f6e3218_master.jpg",
+      "https://product.hstatic.net/200000544803/product/10_be7dc54cb7f24c2c8d3028472c85043c_master.jpg",
+      "https://product.hstatic.net/200000544803/product/14_a15f6a7a4a6347458d6d5ce9d42102c1_master.jpg",
+    ],
+  },
+  {
+    id: 2,
+    type: "Macbook",
+    name: "MacBook Pro M1 2020",
+    serial: "AMOT001-9",
+    brand: "Apple",
+    color: ["gray", "white"],
+    ram_options: [
+      {
+        ram: "8GB",
+        info: [
+          {
+            rom: "256GB",
+            price: 32000000,
+          },
+          {
+            rom: "512GB",
+            price: 33990000,
+          },
+        ],
+      },
+      {
+        ram: "16GB",
+        info: [
+          {
+            rom: "256GB",
+            price: 35999000,
+          },
+          {
+            rom: "512GB",
+            price: 42999000,
+          },
+        ],
+      },
+    ],
+    image: [
+      "https://product.hstatic.net/200000544803/product/6_ae4de0c675944598ae6553b33932574c_master.png",
+      "https://product.hstatic.net/200000544803/product/7_68ae9af150e343298d0c94bc26430398_master.png",
+      "https://product.hstatic.net/200000544803/product/8_34a103c096cb47c48f1e7ffb1eed1834_master.png",
+      "https://product.hstatic.net/200000544803/product/11_f8527e5811bf4f489cd13f594bd0154e_master.png",
+    ],
+  },
+  {
+    id: 3,
+    type: "Iphone",
+    name: "iPhone 11",
+    serial: "AMOT002-1",
+    brand: "Apple",
+    color: ["gray", "white", "golden", "black", "magenta", "purple"],
+    ram_options: [
+      {
+        ram: "4GB",
+        info: [
+          {
+            rom: "64GB",
+            price: 18990000,
+          },
+          {
+            rom: "128GB",
+            price: 19990000,
+          },
+        ],
+      },
+    ],
+    image: [
+      "https://product.hstatic.net/200000544803/product/5_0cb452551d0743a6a430cc753c4b97e1_master.png",
+      "https://product.hstatic.net/200000544803/product/6_1eb57a070fec4692b53137cfa533f804_master.png",
+      "https://product.hstatic.net/200000544803/product/8_cfedc371d4df49a09a86c6d9d2d006f8_master.png",
+      "https://product.hstatic.net/200000544803/product/14_f1a49f44d2a04d6e8bd6f8d602fb960f_master.png",
+    ],
+  },
+  {
+    id: 4,
+    type: "Iphone",
+    name: "iPhone 12",
+    serial: "AMOT002-74",
+    brand: "Apple",
+    color: ["gray", "white", "golden", "black", "magenta", "purple"],
+    ram_options: [
+      {
+        ram: "4GB",
+        info: [
+          {
+            rom: "64GB",
+            price: 22999000,
+          },
+          {
+            rom: "128GB",
+            price: 24999000,
+          },
+          {
+            rom: "256GB",
+            price: 28999000,
+          },
+        ],
+      },
+    ],
+    image: [
+      "https://product.hstatic.net/200000544803/product/6_74c8a72a363d459fbd7e54bd6731639b_master.png",
+      "https://product.hstatic.net/200000544803/product/7_871a5d63d5a64b18a138179fa49b1b5d_master.png",
+      "https://product.hstatic.net/200000544803/product/8_093c6b198b274d72a620a153c5587160_master.png",
+      "https://product.hstatic.net/200000544803/product/9_244f9920f3654af1abc89fe3ea471193_master.png",
+    ],
+  },
+  {
+    id: 5,
+    type: "Iphone",
+    name: "iPhone 13",
+    serial: "AMOT0041-72",
+    brand: "Apple",
+    color: ["gray", "white", "golden"],
+    ram_options: [
+      {
+        ram: "8GB",
+        info: [
+          {
+            rom: "256GB",
+            price: 24990000,
+          },
+          {
+            rom: "512GB",
+            price: 27990000,
+          },
+        ],
+      },
+    ],
+    image: [
+      "https://product.hstatic.net/200000544803/product/iphone_13_green_pdp_position-1b_a7d90e812d004d17812976b95c859ddf_master.png",
+      "https://product.hstatic.net/200000544803/product/iphone_13_green_pdp_position-2_s_a04661f3fc244dfda82a9c2f22701948_master.png",
+      "https://product.hstatic.net/200000544803/product/iphone_13_green_pdp_position-4_d_5c914bd089dd4c8c842e74a3ea7641a5_master.png",
+      "https://product.hstatic.net/200000544803/product/iphone_13_green_pdp_position-9_t_b450ce81f8d24bca89d4019cc88d9096_master.png",
+    ],
+  },
+  {
+    id: 6,
+    type: "Ipad",
+    name: "iPad Air 4",
+    serial: "AMOT041-412",
+    brand: "Apple",
+    color: ["gray", "white", "golden", "magenta", "black", "green"],
+    ram_options: [
+      {
+        ram: "8GB",
+        info: [
+          {
+            rom: "256GB",
+            price: 16990000,
+          },
+          {
+            rom: "512GB",
+            price: 20990000,
+          },
+        ],
+      },
+    ],
+    image: [
+      "https://product.hstatic.net/200000544803/product/2_a884f7f7c3464c1585ea11a83cb425a4_master.png",
+      "https://product.hstatic.net/200000544803/product/3_ab6a029bbe9f4ebf8c8c0391e7a2e1e6_master.png",
+      "https://product.hstatic.net/200000544803/product/14_8e186b1ad8454684acb431cb4bb74d8c_master.png",
+      "https://product.hstatic.net/200000544803/product/111_41d2a9a3fd234490af4c7cf435d1611c_master.png",
+    ],
+  },
+  {
+    id: 7,
+    type: "Ipad",
+    name: "iPad Air 5",
+    serial: "AMOT0021-74",
+    brand: "Apple",
+    color: ["gray", "white", "golden"],
+    ram_options: [
+      {
+        ram: "8GB",
+        info: [
+          {
+            rom: "64GB",
+            price: 17500000,
+          },
+        ],
+      },
+    ],
+    image: [
+      "https://product.hstatic.net/200000544803/product/d_c9a314402eb445ed91f296044aa8b90c_master.png",
+      "https://product.hstatic.net/200000544803/product/ipad_air_cellular_space_gray_pdp_6af6332fe55f46ef82ea8a9de3d95ddc_master.png",
+      "https://product.hstatic.net/200000544803/product/x_42ffebff4e594598b52f213c5e40b688_master.png",
+      "https://product.hstatic.net/200000544803/product/sadasd_fb511cb4ea224a0da020ac7ffd02fe7d_master.png",
+    ],
+  },
+  {
+    id: 8,
+    type: "Ipad",
+    name: "iPad gen 9",
+    serial: "AMOT00145-72",
+    brand: "Apple",
+    color: ["gray", "white"],
+    ram_options: [
+      {
+        ram: "8GB",
+        info: [
+          {
+            rom: "64GB",
+            price: 10990000,
+          },
+          {
+            rom: "256GB",
+            price: 13990000,
+          },
+        ],
+      },
+    ],
+    image: [
+      "https://product.hstatic.net/200000544803/product/2_f5e2c59e1b9e453aa078c1c640fe016f_master.png",
+      "https://product.hstatic.net/200000544803/product/3_bb5c55f65660435fa82a780db46e4bfd_master.png",
+      "https://product.hstatic.net/200000544803/product/4_6e2f37547ced4abd861a26492d201539_master.png",
+      "https://product.hstatic.net/200000544803/product/5_f319c002070540ad93c60b241fd951d4_master.png",
+    ],
+  },
+  {
+    id: 9,
+    type: "Ipad",
+    name: "iPad mini 6 2021",
+    serial: "AMOT00145-72",
+    brand: "Apple",
+    color: ["gray", "white", "pink", "violet"],
+    ram_options: [
+      {
+        ram: "8GB",
+        info: [
+          {
+            rom: "64GB",
+            price: 14900000,
+          },
+          {
+            rom: "256GB",
+            price: 19920000,
+          },
+        ],
+      },
+    ],
+    image: [
+      "https://product.hstatic.net/200000544803/product/11_124d5877446f40328f1741ae4d306f4e_master.png",
+      "https://product.hstatic.net/200000544803/product/13_27c398aae9b44eac8b384b0a34f5f8ac_master.png",
+      "https://product.hstatic.net/200000544803/product/14_c6226676413643818efdbd8e89692500_master.jpg",
+      "https://product.hstatic.net/200000544803/product/15_bbbcb55b7c1e4c6e953e1d08683a9817_master.png",
+    ],
+  },
+  {
+    id: 10,
+    type: "Ipad",
+    name: "iPad Pro 12.9 inch (M1, 2021)",
+    serial: "AMOT00145-72",
+    brand: "Apple",
+    color: ["gray", "white"],
+    ram_options: [
+      {
+        ram: "8GB",
+        info: [
+          {
+            rom: "128",
+            price: 33900000,
+          },
+          {
+            rom: "256GB",
+            price: 37900000,
+          },
+        ],
+      },
+    ],
+    image: [
+      "https://product.hstatic.net/200000544803/product/2_279595eebf0c4fe4a18de944e23f56e2_master.png",
+      "https://product.hstatic.net/200000544803/product/3_7e6204a551f04c359c807e74f001d9d7_master.png",
+      "https://product.hstatic.net/200000544803/product/1_080c613d0b0c48638a1995668549866c_master.png",
+      "https://product.hstatic.net/200000544803/product/6_d73aa2bd5d4c4118a2ec97adbb54a837_master.png",
+    ],
+  },
+];
