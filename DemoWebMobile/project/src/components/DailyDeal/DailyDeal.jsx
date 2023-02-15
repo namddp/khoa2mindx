b23a4ab068c299411430529504bfe7e42139368b
Đức Huy <briannguyen.contact@gmail.com>
2023-02-15 10:26:20 +0700

    update

--- DemoWebMobile/project/src/components/DailyDeal/DailyDeal.jsx
+++ DemoWebMobile/project/src/components/DailyDeal/DailyDeal.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { ProductsData } from '../../data/ProductsData'
-import Slider from 'react-slick'
+import Slider from 'react-slick';
+import CountdownTimer from './CountdownTimer';
 import './DailyDeal.css';
 var settings = {
     dots: true,
@@ -10,6 +11,8 @@ var settings = {
     slidesToShow: 5,
     slidesToScroll: 1,
     initialSlide: 0,
+    // autoplay:true,
+    // autoplaySpeed:5000,
     responsive: [
       {
         breakpoint: 1024,
@@ -39,43 +42,67 @@ var settings = {
   };
 
 const DailyDeal = () => {
-    const [listItem,setListItem] =  useState(ProductsData);
+  const [listItem, setListItem] = useState(ProductsData);
+  const [isCountdownPaused, setIsCountdownPaused] = useState(false);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!isCountdownPaused) {
+        setListItem(prevListItems => {
+          const newItems = Array.from({length: 8}, () => {
+            return ProductsData[Math.floor(Math.random() * ProductsData.length)];
+          });
+          return newItems;
+        });
+      }
+    }, 60000);
+    return () => clearInterval(interval);
+  }, [isCountdownPaused]);
+  const handleCountdownPause = (isPaused) => {
+    setIsCountdownPaused(isPaused);
+    if (isPaused) {
+      setListItem(prevListItems => {
+        const newItems = Array.from({length: 8}, () => {
+          return ProductsData[Math.floor(Math.random() * ProductsData.length)];
+        });
+        return newItems;
+      });
+    }
+  }
   return (
     <>
-    <div className='dailyDeal'>
+       <div className='dailyDeal'>
         <div className='dealTitle'>
-            <h3>Deal Ngon Mỗi Ngày</h3>
-            <h3>69 ngày</h3>
+          <h3>Deal Ngon Mỗi Ngày</h3>
+          <h3><CountdownTimer onCountdownPause={handleCountdownPause} /></h3>
         </div>
         <Slider {...settings}>
-        {listItem.map(e => {
+          {listItem.map(e => {
             return (
-                <div className='dealItem' key={e.id}>
-                    <div className='dealImg'>
-                    <img src={e.image[0]} alt="" />
-                    </div>
-                    <div className='action'>
-                        <span>yêu thích</span>
-                        <span>xem</span>
-                        <span>so sánh</span>
-                    </div>
-                    <div className='itemImg'>
-                        {e.image.map((i,index)=> {
-                            return (
-                                <img src={i} alt="" key={index}/>
-                            )
-                        })}
-                    </div>
-                    <div className='itemInfo'>
-                    
-                    <h4>{e.name}</h4>
-                    <h6>{e.ram_options[0].info[0].price} VND<span>{e.ram_options[0].info[0].price * 0.1} VND</span></h6>
-                    </div>
+              <div className='dealItem' key={e.id}>
+                <div className='dealImg'>
+                  <img src={e.image[0]} alt="" />
                 </div>
+                <div className='action'>
+                  <span>yêu thích</span>
+                  <span>xem</span>
+                  <span>so sánh</span>
+                </div>
+                <div className='itemImg'>
+                  {e.image.map((i, index) => {
+                    return (
+                      <img src={i} alt="" key={index} />
+                    )
+                  })}
+                </div>
+                <div className='itemInfo'>
+                  <h4>{e.name}</h4>
+                  <h6>{e.ram_options[0].info[0].price} VND<span>{e.ram_options[0].info[0].price * 0.1} VND</span></h6>
+                </div>
+              </div>
             )
-        })}
+          })}
         </Slider>
-    </div>
+      </div>
     <div className='productDealList'>
         <div className='productDealItem'>
             <img src="https://theme.hstatic.net/200000544803/1000902479/14/amot_slider_small_img_1.png?v=1880" alt="" />
@@ -94,4 +121,4 @@ const DailyDeal = () => {
   )
 }
 
-export default DailyDeal
+export default DailyDeal  
