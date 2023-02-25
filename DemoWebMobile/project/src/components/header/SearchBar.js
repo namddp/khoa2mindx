aaa8c0d51675baba8a59a6dbc22d8348fa713852
{Huy} <briannguyen.contact@gmail.com>
2023-02-25 15:17:23 +0700

    Update SearchBar.js

--- DemoWebMobile/project/src/components/header/SearchBar.js
+++ DemoWebMobile/project/src/components/header/SearchBar.js
@@ -1,24 +1,25 @@
-import React, { useState ,useEffect} from "react";
-import {ProductsData} from "../../data/ProductsData";
+import React, { useState, useEffect } from "react";
+import { ProductsData } from "../../data/ProductsData";
 import "./SearchBar.css";
 
 function SearchBar() {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [filteredData, setFilteredData] = useState(ProductsData);
+  const [searchTerm, setSearchTerm] = useState("");
 
+  const filtered = ProductsData.filter((p) =>
+    p.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   const handleSearch = (e) => {
     setSearchTerm(e.target.value.toLowerCase());
-
-    
   };
-  useEffect(() => {
-    console.log(filteredData)
-    setFilteredData(
-      ProductsData.filter((product) =>
-        new RegExp(searchTerm, "i").test(product.name)
-      )
-    );
-  },[]);
+
+  const formatPrice = (price) => {
+    const f = new Intl.NumberFormat("vi-vn", {
+      style: "currency",
+      currency: "VND",
+    });
+
+    return f.format(price);
+  };
 
   return (
     <div>
@@ -37,23 +38,22 @@ function SearchBar() {
       </li>
       <ul className="product-list">
         {searchTerm ? (
-          filteredData.length > 0 ? (
-            filteredData.map((product) => (
+          !!filtered.length ? (
+            filtered.map((product) => (
               <li className="product-frame" key={product.id}>
                 <img src={product.image[0]} alt={product.name} />
                 <h3>
                   <a href={product.url}>{product.name}</a>
                 </h3>
                 {/* <hr/> */}
-                <p>{product.ram_options[0].info[0].price}</p>
-                <hr/>
+                <p>{formatPrice(product?.options?.[0]?.info[0]?.price || 0)}</p>
+                <hr />
               </li>
             ))
           ) : (
             <p className="not-found">Không Tìm Thấy Sản Phẩm Liên Quan</p>
           )
         ) : null}
-        
       </ul>
     </div>
   );
