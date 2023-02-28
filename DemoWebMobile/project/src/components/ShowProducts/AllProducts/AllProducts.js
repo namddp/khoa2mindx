f2eb53bf2a78d27b7f99a1e8cf39eeab54aeb7b8
TMHieu <hieu.truongminh.hcm@gmail.com>
2023-02-28 15:20:35 +0700

    a

--- DemoWebMobile/project/src/components/ShowProducts/AllProducts/AllProducts.js
+++ DemoWebMobile/project/src/components/ShowProducts/AllProducts/AllProducts.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./AllProducts.css";
 import { BsPlus, BsEyeFill } from "react-icons/bs";
